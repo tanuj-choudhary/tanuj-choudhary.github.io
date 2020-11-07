f6ad0f56239c7bdf60004fdc22f0392b7b520c65
Tanuj <tanujchoudhary001@gmail.com>
2020-11-07 12:55:24 +0530

    Making downlaod and view button work

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const UIController = (() => {
   const DOMStrings = {
     menu: 'hamburger',
     navList: 'nav-list',
+    viewButton:'btn-view'
   };
 
   return {
@@ -15,12 +16,17 @@ const Controller = (() => {
   const DOMStrings = UIController.getDOMStrings();
 
   setupEventListeners = () => {
-    const { menu, navList } = DOMStrings;
+    const { menu, navList,viewButton } = DOMStrings;
 
     // Togglelist on menu button click
     document.querySelector(`.${menu}`).addEventListener('click', (e) => {
       document.querySelector(`.${navList}`).classList.toggle('collapse');
     });
+
+    // Open my github profile in new tab
+    document.querySelector(`.${viewButton}`).addEventListener('click', function () {
+      window.open("https://github.com/Tanuj-Choudhary/");
+    })
   };
 
   return {
