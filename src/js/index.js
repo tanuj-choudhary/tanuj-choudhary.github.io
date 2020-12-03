8ddabf0bf8563b973dfaed781a6dc1cbb66b6c5b
Tanuj <tanujchoudhary001@gmail.com>
2020-12-03 14:46:35 +0530

    Added mail sending

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,12 @@ const UIController = (() => {
     viewButton: 'btn-view',
     fbFollowLink: 'follow__link__fb',
     twitterFollowLink: 'follow__link__twitter',
-    linkedinFollowLink:'follow__link__linkedin'
+    linkedinFollowLink: 'follow__link__linkedin',
+    formSubmitButton: 'btn-form',
+    nameInput: 'name',
+    subjectInput: 'subject',
+    emailInput: 'email',
+    messageInput:'message'
   };
 
   return {
@@ -34,8 +39,31 @@ const Controller = (() => {
     window.open("https://www.linkedin.com/in/tanuj-choudhary01");
   }
 
+
+  function getFormValues() {
+    const { nameInput, subjectInput, emailInput, messageInput } = DOMStrings;
+
+    const name = document.getElementById(nameInput).value;
+    const subject = document.getElementById(subjectInput).value;
+    const email = document.getElementById(emailInput).value;
+    const message = document.getElementById(messageInput).value;
+
+    const formValues = { name, subject, email, message };
+    
+    return formValues;
+  }
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    const formValues = getFormValues();
+    console.log(formValues);
+
+    window.open(`mailto:tanujchoudhary001@example.com?subject=${formValues.subject}&body=${formValues.message}`);
+  } 
+
   setupEventListeners = () => {
-    const { menu, navList,viewButton,fbFollowLink,twitterFollowLink,linkedinFollowLink } = DOMStrings;
+    const { menu, navList,viewButton,fbFollowLink,twitterFollowLink,linkedinFollowLink,formSubmitButton } = DOMStrings;
 
     // Togglelist on menu button click
     document.querySelector(`.${menu}`).addEventListener('click', (e) => {
@@ -47,6 +75,9 @@ const Controller = (() => {
     document.querySelector(`.${fbFollowLink}`).addEventListener('click', openFb);
     document.querySelector(`.${twitterFollowLink}`).addEventListener('click', openTwitter);
     document.querySelector(`.${linkedinFollowLink}`).addEventListener('click', openLinkedin);
+
+    // Send mail
+    document.querySelector(`.${formSubmitButton}`).addEventListener('click', sendEmail);
   };
 
   return {
