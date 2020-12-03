ede84304cda797b86c0281ea34cdd616016b9a47
Tanuj <tanujchoudhary001@gmail.com>
2020-12-03 20:20:27 +0530

    Prettify index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
 // UI Controller
 const UIController = (() => {
   const DOMStrings = {
-    menu: 'hamburger',
-    navList: 'nav-list',
-    viewButton: 'btn-view',
-    fbFollowLink: 'follow__link__fb',
-    twitterFollowLink: 'follow__link__twitter',
-    linkedinFollowLink: 'follow__link__linkedin',
-    formSubmitButton: 'btn-form',
-    nameInput: 'name',
-    subjectInput: 'subject',
-    emailInput: 'email',
-    messageInput:'message'
+    menu: "hamburger",
+    navList: "nav-list",
+    viewButton: "btn-view",
+    fbFollowLink: "follow__link__fb",
+    twitterFollowLink: "follow__link__twitter",
+    linkedinFollowLink: "follow__link__linkedin",
+    formSubmitButton: "btn-form",
+    nameInput: "name",
+    subjectInput: "subject",
+    emailInput: "email",
+    messageInput: "message",
   };
 
   return {
@@ -23,22 +23,21 @@ const UIController = (() => {
 const Controller = (() => {
   const DOMStrings = UIController.getDOMStrings();
 
-  function openFb()  {
+  function openFb() {
     window.open("https://www.facebook.com/tanuj.choudharyy/");
   }
 
-  function openTwitter()  {
+  function openTwitter() {
     window.open("https://twitter.com/Tanuj_C");
   }
 
-  function openGithub()  {
+  function openGithub() {
     window.open("https://github.com/Tanuj-Choudhary/");
   }
 
-  function openLinkedin()  {
+  function openLinkedin() {
     window.open("https://www.linkedin.com/in/tanuj-choudhary01");
   }
-
 
   function getFormValues() {
     const { nameInput, subjectInput, emailInput, messageInput } = DOMStrings;
@@ -49,7 +48,7 @@ const Controller = (() => {
     const message = document.getElementById(messageInput).value;
 
     const formValues = { name, subject, email, message };
-    
+
     return formValues;
   }
 
@@ -58,25 +57,45 @@ const Controller = (() => {
 
     const formValues = getFormValues();
 
-    window.open(`mailto:tanujchoudhary001@example.com?subject=${formValues.subject}&body=${formValues.message}`);
-  } 
+    window.open(
+      `mailto:tanujchoudhary001@example.com?subject=${formValues.subject}&body=${formValues.message}`
+    );
+  }
 
   setupEventListeners = () => {
-    const { menu, navList,viewButton,fbFollowLink,twitterFollowLink,linkedinFollowLink,formSubmitButton } = DOMStrings;
+    const {
+      menu,
+      navList,
+      viewButton,
+      fbFollowLink,
+      twitterFollowLink,
+      linkedinFollowLink,
+      formSubmitButton,
+    } = DOMStrings;
 
     // Togglelist on menu button click
-    document.querySelector(`.${menu}`).addEventListener('click', (e) => {
-      document.querySelector(`.${navList}`).classList.toggle('collapse');
+    document.querySelector(`.${menu}`).addEventListener("click", (e) => {
+      document.querySelector(`.${navList}`).classList.toggle("collapse");
     });
 
     // Open my profiles in new tab
-    document.querySelector(`.${viewButton}`).addEventListener('click', openGithub);
-    document.querySelector(`.${fbFollowLink}`).addEventListener('click', openFb);
-    document.querySelector(`.${twitterFollowLink}`).addEventListener('click', openTwitter);
-    document.querySelector(`.${linkedinFollowLink}`).addEventListener('click', openLinkedin);
+    document
+      .querySelector(`.${viewButton}`)
+      .addEventListener("click", openGithub);
+    document
+      .querySelector(`.${fbFollowLink}`)
+      .addEventListener("click", openFb);
+    document
+      .querySelector(`.${twitterFollowLink}`)
+      .addEventListener("click", openTwitter);
+    document
+      .querySelector(`.${linkedinFollowLink}`)
+      .addEventListener("click", openLinkedin);
 
     // Send mail
-    document.querySelector(`.${formSubmitButton}`).addEventListener('click', sendEmail);
+    document
+      .querySelector(`.${formSubmitButton}`)
+      .addEventListener("click", sendEmail);
   };
 
   return {
