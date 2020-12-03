7effc53ee082822e1f748e5ca017c44e34de57f3
Tanuj <tanujchoudhary001@gmail.com>
2020-12-03 14:24:27 +0530

    Added follow links

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,10 @@ const UIController = (() => {
   const DOMStrings = {
     menu: 'hamburger',
     navList: 'nav-list',
-    viewButton:'btn-view'
+    viewButton: 'btn-view',
+    fbFollowLink: 'follow__link__fb',
+    twitterFollowLink: 'follow__link__twitter',
+    linkedinFollowLink:'follow__link__linkedin'
   };
 
   return {
@@ -15,18 +18,35 @@ const UIController = (() => {
 const Controller = (() => {
   const DOMStrings = UIController.getDOMStrings();
 
+  function openFb()  {
+    window.open("https://www.facebook.com/tanuj.choudharyy/");
+  }
+
+  function openTwitter()  {
+    window.open("https://twitter.com/Tanuj_C");
+  }
+
+  function openGithub()  {
+    window.open("https://github.com/Tanuj-Choudhary/");
+  }
+
+  function openLinkedin()  {
+    window.open("https://www.linkedin.com/in/tanuj-choudhary01");
+  }
+
   setupEventListeners = () => {
-    const { menu, navList,viewButton } = DOMStrings;
+    const { menu, navList,viewButton,fbFollowLink,twitterFollowLink,linkedinFollowLink } = DOMStrings;
 
     // Togglelist on menu button click
     document.querySelector(`.${menu}`).addEventListener('click', (e) => {
       document.querySelector(`.${navList}`).classList.toggle('collapse');
     });
 
-    // Open my github profile in new tab
-    document.querySelector(`.${viewButton}`).addEventListener('click', function () {
-      window.open("https://github.com/Tanuj-Choudhary/");
-    })
+    // Open my profiles in new tab
+    document.querySelector(`.${viewButton}`).addEventListener('click', openGithub);
+    document.querySelector(`.${fbFollowLink}`).addEventListener('click', openFb);
+    document.querySelector(`.${twitterFollowLink}`).addEventListener('click', openTwitter);
+    document.querySelector(`.${linkedinFollowLink}`).addEventListener('click', openLinkedin);
   };
 
   return {
